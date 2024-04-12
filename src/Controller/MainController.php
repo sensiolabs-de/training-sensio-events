868902ffbe2d5f152b2da8cc1aa72e6b6ee99a6c
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-04-12 10:09:58 +0200

    [START][Static-Routing] Exercise 3 - Route on controller

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -5,9 +5,11 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class MainController extends AbstractController
 {
+    #[Route('/', name: 'app_main_index')]
     public function index(Request $request): Response
     {
         $name = $request->query->get('name', 'World');
