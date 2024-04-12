defa9b2885df7bac7045e5f27399497c5249db0c
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-04-12 08:58:02 +0200

    [START][Static-Routing] Exercise 1 - MainController

---
+++ src/Controller/MainController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class MainController extends AbstractController
+{
+    public function index(): Response
+    {
+        return new Response('Hello World');
+    }
+}
