010f2713302a46dcd7a8e2614806e96f0cce0726
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-04-29 09:01:36 +0200

    [START][Display-Data] Exercise 1 - Create Repository method

--- src/Repository/EventRepository.php
+++ src/Repository/EventRepository.php
@@ -21,6 +21,27 @@ class EventRepository extends ServiceEntityRepository
         parent::__construct($registry, Event::class);
     }
 
+    public function findEventsBetweenDates(?\DateTimeImmutable $start = null, ?\DateTimeImmutable $end = null): array
+    {
+        if (null === $start && null === $end) {
+            throw new \InvalidArgumentException('At least one date is required to operate this method.');
+        }
+
+        $qb = $this->createQueryBuilder('e');
+
+        if ($start instanceof \DateTimeImmutable) {
+            $qb->andWhere('e.startAt >= :start')
+                ->setParameter('start', $start);
+        }
+
+        if ($end instanceof \DateTimeImmutable) {
+            $qb->andWhere('e.endAt <= :end')
+                ->setParameter('end', $end);
+        }
+
+        return $qb->getQuery()->getResult();
+    }
+
     //    /**
     //     * @return Event[] Returns an array of Event objects
     //     */
