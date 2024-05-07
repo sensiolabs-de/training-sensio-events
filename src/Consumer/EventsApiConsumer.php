179e2f5c8d2356c20d851a1e6679ac693c75f4c3
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-05-07 17:51:05 +0200

    [MASTER][HttpClient] Exercise 2 - Inject HttpClient in Consumer

--- src/Consumer/EventsApiConsumer.php
+++ src/Consumer/EventsApiConsumer.php
@@ -4,11 +4,12 @@ namespace App\Consumer;
 
 use App\Search\EventSearchInterface;
 use Symfony\Component\DependencyInjection\Attribute\Autowire;
-use Symfony\Component\HttpClient\HttpClient;
+use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class EventsApiConsumer implements EventSearchInterface
 {
     public function __construct(
+        private readonly HttpClientInterface $client,
         #[Autowire(param: 'env(EVENTS_API_KEY)')]
         private readonly string $apiKey,
     )
@@ -17,9 +18,7 @@ class EventsApiConsumer implements EventSearchInterface
 
     public function searchByName(?string $name = null): array
     {
-        $client = HttpClient::create();
-
-        return $client->request('GET', 'https://www.devevents-api.fr/api/events', [
+        return $this->client->request('GET', 'https://www.devevents-api.fr/api/events', [
             'query' => ['name' => $name],
             'headers' => [
                 'apikey' => $this->apiKey,
