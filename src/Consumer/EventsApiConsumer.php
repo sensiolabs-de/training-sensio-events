138b925e41e7719c0d330b77452416723a861597
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-05-07 17:51:48 +0200

    [MASTER][HttpClient] Exercise 3 - Use Scoped Client in Consumer

--- src/Consumer/EventsApiConsumer.php
+++ src/Consumer/EventsApiConsumer.php
@@ -3,27 +3,20 @@
 namespace App\Consumer;
 
 use App\Search\EventSearchInterface;
-use Symfony\Component\DependencyInjection\Attribute\Autowire;
 use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class EventsApiConsumer implements EventSearchInterface
 {
     public function __construct(
-        private readonly HttpClientInterface $client,
-        #[Autowire(param: 'env(EVENTS_API_KEY)')]
-        private readonly string $apiKey,
+        private readonly HttpClientInterface $eventsClient,
     )
     {
     }
 
     public function searchByName(?string $name = null): array
     {
-        return $this->client->request('GET', 'https://www.devevents-api.fr/api/events', [
+        return $this->eventsClient->request('GET', 'https://www.devevents-api.fr/api/events', [
             'query' => ['name' => $name],
-            'headers' => [
-                'apikey' => $this->apiKey,
-                'Accept' => 'application/json',
-            ],
         ])->toArray();
     }
 }
