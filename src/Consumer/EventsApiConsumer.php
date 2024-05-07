0059496adb9d76e1953f8bf821d593eb9b54ccf0
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-05-07 09:12:44 +0200

    [MASTER][Dep-Inj] Exercise 7 - Inject env var in Consumer

--- src/Consumer/EventsApiConsumer.php
+++ src/Consumer/EventsApiConsumer.php
@@ -3,9 +3,16 @@
 namespace App\Consumer;
 
 use App\Search\EventSearchInterface;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
 
 class EventsApiConsumer implements EventSearchInterface
 {
+    public function __construct(
+        #[Autowire(param: 'env(EVENTS_API_KEY)')]
+        private readonly string $apiKey,
+    )
+    {
+    }
 
     public function searchByName(?string $name = null): array
     {
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -2,16 +2,29 @@
 
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+    public function __construct(
+        protected readonly UserPasswordHasherInterface $hasher,
+        #[Autowire(param: 'env(ADMIN_PWD)')]
+        protected readonly string $adminPwd,
+    ) {
+    }
+
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $user = (new User())
+            ->setEmail('benjamin.zaslavsky@gmail.com')
+            ->setRoles(['ROLE_USER', 'ROLE_ADMIN']);
+        $user->setPassword($this->hasher->hashPassword($user, $this->adminPwd));
 
+        $manager->persist($user);
         $manager->flush();
     }
 }
