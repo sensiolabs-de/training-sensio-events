7818ea42798303a084f14e9c08ea3eb3920904b6
Benjamin Zaslavsky <benjamin.zaslavsky@gmail.com>
2024-05-17 09:22:16 +0200

    [MASTER][Serialization] Exercise 1 - Return JsonResponse in API endpoint

--- src/Controller/Api/VolunteerController.php
+++ src/Controller/Api/VolunteerController.php
@@ -4,16 +4,25 @@ namespace App\Controller\Api;
 
 use App\Repository\VolunteerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use Symfony\Component\Security\Http\Attribute\IsGranted;
+use Symfony\Component\Serializer\SerializerInterface;
 
 class VolunteerController extends AbstractController
 {
     #[IsGranted('IS_AUTHENTICATED')]
     #[Route('/api/volunteers', name: 'app_api_volunteers')]
-    public function getVolunteersApi(VolunteerRepository $repository): array
+    public function getVolunteersApi(Request $request, VolunteerRepository $repository, SerializerInterface $serializer): Response
     {
-        return $repository->findAll();
+        $limit = 20;
+        $page = $request->query->getInt('page', 1);
+        $volunteers = $repository->findBy([], [], $limit, ($page - 1) * $limit);
+
+        $data =  $serializer->serialize($volunteers, 'json');
+
+        return new JsonResponse($data, Response::HTTP_OK, json: true);
     }
 }
